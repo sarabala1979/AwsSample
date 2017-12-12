f65b369921f0f4281e95bd4b689f8b949c4b1b3e
samrithi <samrithi@DESKTOP-N9J6V6P>
2017-12-12 08:06:34 -0800

    Restored previous version

--- AWSPoc/src/com/poc/common/AWSConfig.java
+++ AWSPoc/src/com/poc/common/AWSConfig.java
@@ -10,9 +10,28 @@ import com.amazonaws.services.sqs.AmazonSQSClient;
 
 public class AWSConfig {
 	
+	private static AWSConfig obj;
+	private AmazonS3 s3client;
+	private AmazonSQS sqs;
+
 	private AWSConfig() {
 		AWSCredentials credentials = new BasicAWSCredentials("AKIAIXC5224TGDLD5RXA", "s6/5mEjd+lzW7BP/oJFzaTD+oQeidxBmrmaaeGLN");
 		AmazonS3 s3client = new AmazonS3Client(credentials);
+	}
+
+	public static AWSConfig getInstance() {
+		if(obj == null) {
+			obj = new AWSConfig();
+		}
+		return obj;
+	}
+
+	public AmazonS3 getAmazonS3() {
+		return s3client;
+	}
+
+	public  AmazonSQS getSQS() {
+		return sqs;
 	}
 	
 
