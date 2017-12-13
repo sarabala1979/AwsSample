b787fa260abf8328f148c7091e96ad8b2ac41515
samrithi <samrithi@DESKTOP-N9J6V6P>
2017-12-13 12:16:54 -0800

    removed

--- AWSLambdaPoc/src/com/poc/common/AWSConfig.java
+++ AWSLambdaPoc/src/com/poc/common/AWSConfig.java
@@ -11,7 +11,7 @@ public class AWSConfig {
 	private AmazonS3 s3client;
 	private AmazonSQS sqs;
 	private AWSConfig() {
-		AWSCredentials credentials = new BasicAWSCredentials("AKIAIXC5224TGDLD5RXA", "s6/5mEjd+lzW7BP/oJFzaTD+oQeidxBmrmaaeGLN");
+		AWSCredentials credentials = new BasicAWSCredentials("", "");
 		sqs = new AmazonSQSClient(credentials);
 		sqs.setEndpoint("https://sqs.us-west-1.amazonaws.com/930471605917");
 		
